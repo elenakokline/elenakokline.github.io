eb6bf28dc52f2f9589c9dff3d1b63085497b8372
6d486f49 <76097bcc@gmail.com>
2025-09-21 13:59:00 -0400

    Removing GA Test

--- Lydia/Program.cs
+++ Lydia/Program.cs
@@ -2,7 +2,6 @@ using Lydia;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using Blazor.Analytics;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-builder.Services.AddGoogleAnalytics("G-8RXPFTJQRP");
 
 await builder.Build().RunAsync();
